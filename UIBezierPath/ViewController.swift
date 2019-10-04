52b5e14989e889ff3c815f23de41d9ed253dbad5
Zedd0202 <ch41150@hanmail.net>
2019-10-04 14:44:32 +0900

    add cnter variable

--- UIBezierPath/ViewController.swift
+++ UIBezierPath/ViewController.swift
@@ -24,21 +24,22 @@ class ZeddView: UIView {
     
     override func draw(_ rect: CGRect) {
         
-        //x degree = x * π / 180 radian
+        let center = CGPoint(x: rect.midX, y: rect.midY)
         let colors = [UIColor.orange, UIColor.black, UIColor.systemGreen, UIColor.systemPink, UIColor.cyan, UIColor.systemTeal]
 
         let values: [CGFloat] = [10, 20, 70]
         let total = values.reduce(0, +)
-
+        
+        //x degree = x * π / 180 radian
         var startAngle: CGFloat = (-(.pi) / 2)
         var endAngle: CGFloat = 0.0
-
+        
         values.forEach { (value) in
             endAngle = (value / total) * (.pi * 2)
             
             let path = UIBezierPath()
-            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
-            path.addArc(withCenter: CGPoint(x: rect.midX, y: rect.midY),
+            path.move(to: center)
+            path.addArc(withCenter: center,
                         radius: 60,
                         startAngle: startAngle,
                         endAngle: startAngle + endAngle,
