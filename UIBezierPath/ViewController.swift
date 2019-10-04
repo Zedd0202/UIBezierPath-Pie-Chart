6ace6bf3d460f97ebff0a22cad07d5eeebf3e3f0
Zedd0202 <ch41150@hanmail.net>
2019-10-04 14:41:30 +0900

    first commit

---
+++ UIBezierPath/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  UIBezierPath
+//
+//  Created by Zedd on 27/09/2019.
+//  Copyright © 2019 Zedd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let view = ZeddView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        view.center = self.view.center
+        view.backgroundColor = .clear
+        self.view.addSubview(view)
+    }
+}
+
+class ZeddView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+        //x degree = x * π / 180 radian
+        let colors = [UIColor.orange, UIColor.black, UIColor.systemGreen, UIColor.systemPink, UIColor.cyan, UIColor.systemTeal]
+
+        let values: [CGFloat] = [10, 20, 70]
+        let total = values.reduce(0, +)
+
+        var startAngle: CGFloat = (-(.pi) / 2)
+        var endAngle: CGFloat = 0.0
+
+        values.forEach { (value) in
+            endAngle = (value / total) * (.pi * 2)
+            
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+            path.addArc(withCenter: CGPoint(x: rect.midX, y: rect.midY),
+                        radius: 60,
+                        startAngle: startAngle,
+                        endAngle: startAngle + endAngle,
+                        clockwise: true)
+            
+            colors.randomElement()?.set()
+            path.fill()
+            startAngle += endAngle
+        }
+    }
+}
