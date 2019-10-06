160dd460991c98f318810c5bb68f1d443bf6980e
Zedd0202 <ch41150@hanmail.net>
2019-10-06 16:03:26 +0900

    add unitTest

---
+++ UIBezierPathTests/UIBezierPathTests.swift
@@ -0,0 +1,41 @@
+//
+//  UIBezierPathTests.swift
+//  UIBezierPathTests
+//
+//  Created by Zedd on 06/10/2019.
+//  Copyright © 2019 Zedd. All rights reserved.
+//
+
+import XCTest
+
+class UIBezierPathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testTrue() {
+        XCTAssertTrue(true)
+    }
+    
+    func testFalse() {
+        XCTAssertTrue(false)
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
